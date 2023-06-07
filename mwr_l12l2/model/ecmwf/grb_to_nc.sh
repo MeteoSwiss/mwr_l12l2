288629494d4f2a48be9c44792d7526c6ddfde608
rruefenacht <rruefenacht@users.noreply.github.com>
2023-06-07 16:47:06 +0200

    add shell script to transform grib to netcdf using func from libeccodes-tools

---
+++ mwr_l12l2/model/ecmwf/grb_to_nc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# transform GRIB file of ECMWF forecast to NetCDF.
+#
+# This step is needed as cfgrib cannot decode a file containing vars with different dims (in this case lnsp vs q and T)
+# TODO: loop over all new grb forecast files (not needed for z from analysis)
+
+GRB_FILE=mwr_l12l2/data/ecmwf_fc/ecmwf_fc_0-20000-0-06610_A_202304250000.grb
+NC_FILE=mwr_l12l2/data/ecmwf_fc/ecmwf_fc_0-20000-0-06610_A_202304250000_converted_to.nc
+
+grib_to_netcdf -k 4 -o $NC_FILE $GRB_FILE
