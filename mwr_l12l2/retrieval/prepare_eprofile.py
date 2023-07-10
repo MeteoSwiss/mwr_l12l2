787aef8b711cdb87d0131517254a5faf5162c14d
rruefenacht <rruefenacht@users.noreply.github.com>
2023-07-10 21:06:16 +0200

    select times for retrieval in prepare_eprofile.py

--- mwr_l12l2/retrieval/prepare_eprofile.py
+++ mwr_l12l2/retrieval/prepare_eprofile.py
@@ -3,22 +3,37 @@ import numpy as np
 import xarray as xr
 
 def main(mwr_files_in, mwr_file_out, alc_files_in=None, alc_file_out=None, delete_mwr_in=False,
-         min_time=None, max_time=None):
+         start_time=None, end_time=None):
     """function preparing E-PROFILE MWR and ALC inputs"""
 
-    min_time = np.datetime64(min_time)
-    max_time = np.datetime64(max_time)
+    tolerance_alc_time = np.timedelta64(5, 'm')
 
+    start_time = np.datetime64(start_time)
+    end_time = np.datetime64(end_time)
+
+    # MWR treatment
     mwr = get_data(mwr_files_in)
+
+    time_min = max(mwr.time.min(), start_time)
+    time_max = min(mwr.time.max(), end_time)
+    mwr = mwr.where((mwr.time > time_min) & (mwr.time < time_max))  # brackets because of precedence of & over > and <
+
     mwr.to_netcdf(mwr_file_out)
     if delete_mwr_in:
         for file in mwr_files_in:
             os.remove(file)
 
+    # ALC treatment
     if alc_files_in:  # not empty list, not None
         # careful: MeteoSwiss daily concat files have problem with calendar. Use instant files or files concat at CEDA
         alc = get_data(alc_files_in)
+        alc = alc.where((alc.time > time_min-tolerance_alc_time) & (alc.time < time_max+tolerance_alc_time))
         alc.to_netcdf(alc_file_out)
+
+    # TODO: return time_min, time_max whether ALC data is not empty and not NaN. CARE: cbh is not good for this as it is also NaN in case of clear sky
+    # TODO: return whether MWR has surface T, RH, p
+    # TODO: maybe best do this with an instance of class. ask Dani
+
 
 def get_data(files_in, concat_dim='time'):
     """read (several) NetCDF input files to a :class:`xarray.Dataset` and fix time encoding for correct nc output"""
