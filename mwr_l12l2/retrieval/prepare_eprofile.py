d533b5c3e73150f51b87edbe1c0e27ea214873a2
rruefenacht <rruefenacht@users.noreply.github.com>
2023-07-10 19:47:11 +0200

    refactor prepare_eprofile.py

--- mwr_l12l2/retrieval/prepare_eprofile.py
+++ mwr_l12l2/retrieval/prepare_eprofile.py
@@ -2,30 +2,35 @@ import os
 import numpy as np
 import xarray as xr
 
-def main(mwr_files_in, mwr_file_out, alc_files_in=None, alc_file_out=None, delete_mwr_in=False):
+def main(mwr_files_in, mwr_file_out, alc_files_in=None, alc_file_out=None, delete_mwr_in=False,
+         min_time=None, max_time=None):
     """function preparing E-PROFILE MWR and ALC inputs"""
 
-    concat_to_single_file(mwr_files_in, mwr_file_out)
+    min_time = np.datetime64(min_time)
+    max_time = np.datetime64(max_time)
+
+    mwr = get_data(mwr_files_in)
+    mwr.to_netcdf(mwr_file_out)
     if delete_mwr_in:
         for file in mwr_files_in:
             os.remove(file)
 
     if alc_files_in:  # not empty list, not None
         # careful: MeteoSwiss daily concat files have problem with calendar. Use instant files or files concat at CEDA
-        concat_to_single_file(alc_files_in, alc_file_out)
+        alc = get_data(alc_files_in)
+        alc.to_netcdf(alc_file_out)
 
-
-def concat_to_single_file(files_in, file_out, concat_dim='time'):
-    """concatenate several NetCDF input files to a single NetCDF output file"""
+def get_data(files_in, concat_dim='time'):
+    """read (several) NetCDF input files to a :class:`xarray.Dataset` and fix time encoding for correct nc output"""
     # TODO: find out why xarray produces all input arrays as dask array and why to_netcdf() produces warning on encoding
-    x = xr.open_mfdataset(files_in, concat_dim=concat_dim, combine='nested')
-    x = drop_duplicates(x, dim='time')
+    data = xr.open_mfdataset(files_in, concat_dim=concat_dim, combine='nested')
+    data = drop_duplicates(data, dim=concat_dim)
 
     # correct time encoding (especially units) which is broken by open_mfdateset by explicitly loading first file
-    y = xr.open_dataset(files_in[0])
-    x.time.encoding = y.time.encoding
+    data_first = xr.open_dataset(files_in[0])
+    data.time.encoding = data_first.time.encoding
 
-    x.to_netcdf(file_out)
+    return data
 
 
 def drop_duplicates(ds, dim):
