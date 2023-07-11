da70de881b42e978d9fe5f378c4775a1757a2de5
rruefenacht <rruefenacht@users.noreply.github.com>
2023-07-11 10:33:11 +0200

    truely drop data outside of time limits

--- mwr_l12l2/retrieval/prepare_eprofile.py
+++ mwr_l12l2/retrieval/prepare_eprofile.py
@@ -16,21 +16,26 @@ def main(mwr_files_in, mwr_file_out, alc_files_in=None, alc_file_out=None, delet
 
     time_min = max(mwr.time.min(), start_time)
     time_max = min(mwr.time.max(), end_time)
-    mwr = mwr.where((mwr.time > time_min) & (mwr.time < time_max))  # brackets because of precedence of & over > and <
+    mwr = mwr.where((mwr.time >= time_min) & (mwr.time <= time_max), drop=True)  # brackets because of precedence of & over > and <
 
     mwr.to_netcdf(mwr_file_out)
     if delete_mwr_in:
         for file in mwr_files_in:
             os.remove(file)
 
+    if not mwr.time:
+        # TODO: logger.warning; set mwr_data_exists to False
+        return
+
     # ALC treatment
     if alc_files_in:  # not empty list, not None
         # careful: MeteoSwiss daily concat files have problem with calendar. Use instant files or files concat at CEDA
         alc = get_data(alc_files_in)
-        alc = alc.where((alc.time > time_min-tolerance_alc_time) & (alc.time < time_max+tolerance_alc_time))
+        alc = alc.where((alc.time >= time_min-tolerance_alc_time)
+                        & (alc.time <= time_max+tolerance_alc_time), drop=True)
         alc.to_netcdf(alc_file_out)
 
-    # TODO: return time_min, time_max whether ALC data is not empty and not NaN. CARE: cbh is not good for this as it is also NaN in case of clear sky
+    # TODO: return time_min, time_max whether ALC data is not empty and not NaN.
     # TODO: return whether MWR has surface T, RH, p
     # TODO: maybe best do this with an instance of class. ask Dani
 
