d05380a8ebc15edf0ffa55076ac40073774a8222
rruefenacht <rruefenacht@users.noreply.github.com>
2023-07-12 08:55:01 +0200

    add global attributes to ECMWF input files to TROPoe

--- mwr_l12l2/retrieval/interpret_ecmwf.py
+++ mwr_l12l2/retrieval/interpret_ecmwf.py
@@ -131,44 +131,65 @@ class ModelInterpreter(object):
     def produce_tropoe_files(self):  # TODO: extract this method to retrieval class, call interpret_ecmwf.py from retrieval class
         """write reference profile and uncertainties as well as surface data to output files readable by TROPoe"""
         # TODO: make this method more generic
-        # TODO: add some metadata using attrs=... in dict(....)
+
+
         time_encoding = {'units': 'seconds since 1970-01-01', 'calendar': 'standard'}
         prof_data_specs={'base_time': dict(dims=(), data=np.datetime64('1970-01-01', 'ns')),
-                         'lat': dict(dims=(), data=self.fc.latitude.values[int(len(self.fc.latitude)/2)]),
-                         'lon': dict(dims=(), data=self.fc.longitude.values[int(len(self.fc.latitude)/2)]),
                          'time_offset': dict(dims='time', data=self.time_ref),
-                         'height': dict(dims='height', data=self.z_ref/1e3),
+                         'lat': dict(dims=(), data=self.fc.latitude.values[int(len(self.fc.latitude)/2)],
+                                     attrs={'units': 'degrees_north'}),
+                         'lon': dict(dims=(), data=self.fc.longitude.values[int(len(self.fc.latitude)/2)],
+                                     attrs={'units': 'degrees_east'}),
+                         'height': dict(dims='height', data=self.z_ref/1e3,
+                                        attrs={'long_name': 'Height above mean sea level', 'units': 'km'}),
                          'temperature': dict(dims=('time', 'height'), data=self.t_ref[np.newaxis, :]-273.15,
-                                             attrs={'units': 'degrees C'}),
-                         'sigma_temperature': dict(dims=('time', 'height'), data=self.t_err[np.newaxis, :]),
-                         'waterVapor': dict(dims=('time', 'height'), data=self.q_ref[np.newaxis, :]*1e3),
-                         'sigma_waterVapor': dict(dims=('time', 'height'), data=self.q_err[np.newaxis, :]*1e3),
+                                             attrs={'units': 'Celsius'}),
+                         'sigma_temperature': dict(dims=('time', 'height'), data=self.t_err[np.newaxis, :],
+                                                   attrs={'units': 'Celsius'}),
+                         'waterVapor': dict(dims=('time', 'height'), data=self.q_ref[np.newaxis, :]*1e3,
+                                            attrs={'units': 'g/kg'}),
+                         'sigma_waterVapor': dict(dims=('time', 'height'), data=self.q_err[np.newaxis, :]*1e3,
+                                                  attrs={'units': 'g/kg'}),
                          }
-
+        prof_data_attrs = {'source': 'reference profile and uncertainties extracted from ECMWF operational forecast'}
+        #TODO: add more detail on which ECMWF forecast is used to output file directly in main retrieval routine
+        # (info cannot be found inside grib file). Might also want to add lat/lon area used.
 
         #TODO: important! and easy... instead of just taking lowest altitude interp/extrapolate to station_altitude
         # instead. use log for pressure
         sfc_data_specs = {'base_time': dict(dims=(), data=np.datetime64('1970-01-01', 'ns')),
-                          'lat': dict(dims=(), data=self.fc.latitude.values[int(len(self.fc.latitude) / 2)]),
-                          'lon': dict(dims=(), data=self.fc.longitude.values[int(len(self.fc.latitude) / 2)]),
                           'time_offset': dict(dims='time', data=self.time_ref),
-                          'height': dict(dims='height', data=self.z_ref[-1:] / 1e3),
-                          'temperature': dict(dims=('time', 'height'), data=self.t_ref[np.newaxis, -1:] - 273.15),
-                          'sigma_temperature': dict(dims=('time', 'height'), data=self.t_err[np.newaxis, -1:]),
-                          'waterVapor': dict(dims=('time', 'height'), data=self.q_ref[np.newaxis, -1:] * 1e3),
-                          'sigma_waterVapor': dict(dims=('time', 'height'), data=self.q_err[np.newaxis, -1:] * 1e3),
+                          'lat': dict(dims=(), data=self.fc.latitude.values[int(len(self.fc.latitude) / 2)],
+                                      attrs={'units': 'degrees_north'}),
+                          'lon': dict(dims=(), data=self.fc.longitude.values[int(len(self.fc.latitude) / 2)],
+                                      attrs={'units': 'degrees_east'}),
+                          'height': dict(dims='height', data=self.z_ref[-1:] / 1e3,
+                                         attrs={'long_name': 'Height above mean sea level', 'units': 'km'}),
+                          'temperature': dict(dims=('time', 'height'), data=self.t_ref[np.newaxis, -1:] - 273.15,
+                                              attrs={'units': 'Celsius'}),
+                          'sigma_temperature': dict(dims=('time', 'height'), data=self.t_err[np.newaxis, -1:],
+                                                    attrs={'units': 'Celsius'}),
+                          'waterVapor': dict(dims=('time', 'height'), data=self.q_ref[np.newaxis, -1:] * 1e3,
+                                             attrs={'units': 'g/kg'}),
+                          'sigma_waterVapor': dict(dims=('time', 'height'), data=self.q_err[np.newaxis, -1:] * 1e3,
+                                                   attrs={'units': 'g/kg'}),
                           }
+        sfc_data_attrs = {'source': 'surface quantities and uncertainties extracted from ECMWF operational forecast'}
 
+        # construct datasets
         prof_data = xr.Dataset.from_dict(prof_data_specs)
         sfc_data = xr.Dataset.from_dict(sfc_data_specs)
 
-        for ds in [prof_data, sfc_data]:
+        # add encodings and global attrs to datasets
+        for ds in [prof_data, sfc_data]:  #c ommon time encodings for all datasets
             ds = set_encoding(ds, ['base_time', 'time_offset'], time_encoding)
+        prof_data.attrs = prof_data_attrs
+        sfc_data.attrs = sfc_data_attrs
 
+        # save
         prof_data.to_netcdf(self.file_prof_out)
         sfc_data.to_netcdf(self.file_sfc_out)
 
-        # TODO: add global attribute on data source (ECMWF) for the above 2 files
 
     def virt_temp(self):
         """return virtual temperature from temperature and specific humdity in self.fc"""
