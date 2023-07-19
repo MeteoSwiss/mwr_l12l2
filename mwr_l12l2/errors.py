3e8d66e04776ee337795f4f50d3740e5c53f8b33
rruefenacht <rruefenacht@users.noreply.github.com>
2023-07-19 11:25:54 +0200

    flake8

--- mwr_l12l2/errors.py
+++ mwr_l12l2/errors.py
@@ -2,11 +2,14 @@ class MWRError(Exception):
     """Base exception for MWR"""
 
 
+###############################
 class MWRInputError(MWRError):
     """Base exception for calling of MWR functions"""
 
+
 class MWRDataError(MWRError):
     """Raised if something with the input data goes wrong"""
+
 
 class MWRConfigError(MWRError):
     """Raised if something with the configuration file is wrong"""
@@ -15,6 +18,7 @@ class MWRConfigError(MWRError):
 ###############################
 class MissingConfig(MWRConfigError):
     """Raised if a mandatory entry of the config file is missing"""
+
 
 ###############################
 class MissingDataError(MWRDataError):
