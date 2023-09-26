494f55f15cb2ef6efe52f10003baf13fa09a5b7a
rruefenacht <rruefenacht@users.noreply.github.com>
2023-09-26 07:03:51 +0200

    move tropoe runner to tropoe_herpers

--- mwr_l12l2/retrieval/tropoe_helpers.py
+++ mwr_l12l2/retrieval/tropoe_helpers.py
@@ -1,7 +1,10 @@
+from subprocess import run
+
 import numpy as np
 import xarray as xr
 
 from mwr_l12l2.utils.data_utils import set_encoding
+from mwr_l12l2.utils.file_utils import abs_file_path
 
 
 def model_to_tropoe(model):
@@ -73,3 +76,42 @@ def model_to_tropoe(model):
     sfc_data.attrs = sfc_data_attrs
 
     return prof_data, sfc_data
+
+
+def run_tropoe(data_path, date, vip_file, apriori_file, tropoe_img='davidturner53/tropoe',
+               tmp_path='mwr_l12l2/retrieval/tmp', verbosity=1):
+    """Run TROPoe container using podman for one specific retrieval
+
+    Args:
+        data_path: path that will be mounted to /data inside the container. Absolute path or relative to project dir
+        date: date for which retrieval shall be executed. For now retrievals cannot encompass more than one day.
+            Make sure that it is of type :class:`datetime.datetime` or a string of type 'yyyymmdd'. Alternatively you
+            can pass 0 or '0' to let TROPoe print back the vip-file parameter options.
+        vip_file: path to vip file relative to :obj:`data_path`
+        apriori_file:  path to a-priori file relative to :obj:`data_path`
+        tropoe_img (optional): reference of TROPoe continer image to use. Will take latest available by default
+        tmp_path (optional): tmp path that will be mounted to /tmp inside the container. Uses a dummy folder by default
+        verbosity (optional): verbosity level of TROPoe. Defaults to 1
+    """
+
+    # generate date string. Accept datetime.datetime and strings/integers (for special calls, e.g. 0 for vip docs)
+    try:
+        date_str = date.strftime('%Y%m%d')
+    except AttributeError:
+        date_str = '{}'.format(date)  # format to handle also integer input
+
+    cmd = ['podman', 'run', '-i', '-u', 'root', '--rm',
+           '-v', '{}:/data'.format(abs_file_path(data_path)),  # map the data path to /data inside the container
+           '-v', '{}:/tmp2'.format(abs_file_path(tmp_path)),  # map the tmp path to /tmp2 (for debug only)
+           '-e', 'yyyymmdd=' + date_str,
+           '-e', 'vfile=/data/' + vip_file,  # path inside container, e.g. relative to dir mapped to /data
+           '-e', 'pfile=/data/' + apriori_file,  # path inside container, e.g. relative to dir mapped to /data
+           '-e', 'shour=00',  # achieve time selection over input files, hence consider whole day here
+           '-e', 'ehour=24',  # achieve time selection over input files, hence consider whole day here
+           '-e', 'verbose={}'.format(verbosity),
+           tropoe_img]
+    run(cmd)
+
+
+if __name__ == '__main__':
+    run_tropoe('mwr_l12l2/data', 0, 'dummy/vip.txt', 'dummy/Xa_Sa.Lindenberg.55level.08.cdf')
