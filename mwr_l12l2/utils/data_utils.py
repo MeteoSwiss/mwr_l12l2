a7266c5e8ce16ba81bd08c8a5f677babe46de0c3
leric2 <e.sauvageat@gmail.com>
2025-11-26 13:16:50 +0000

    improved function to read L1

--- mwr_l12l2/utils/data_utils.py
+++ mwr_l12l2/utils/data_utils.py
@@ -3,17 +3,35 @@ import pandas as pd
 import xarray as xr
 
 
+# def get_from_nc_files(files_in, concat_dim='time'):
+#     """read (several) NetCDF input files to a :class:`xarray.Dataset` and fix time encoding for correct nc output"""
+#     data = xr.open_mfdataset(files_in, concat_dim=concat_dim, combine='nested')
+#     data = drop_duplicates(data, dim=concat_dim)
+
+#     # correct time encoding (especially units) which is broken by open_mfdateset by explicitly loading first file
+#     data_first = xr.open_dataset(files_in[0])
+#     data = set_encoding(data, ['time'], data_first.time.encoding)
+
+#     return data
+
 def get_from_nc_files(files_in, concat_dim='time'):
-    """read (several) NetCDF input files to a :class:`xarray.Dataset` and fix time encoding for correct nc output"""
-    data = xr.open_mfdataset(files_in, concat_dim=concat_dim, combine='nested')
-    data = drop_duplicates(data, dim=concat_dim)
+    """Read a list of MWR L1 E-Profile files and return a xarray dataset"""
 
-    # correct time encoding (especially units) which is broken by open_mfdateset by explicitly loading first file
-    data_first = xr.open_dataset(files_in[0])
-    data = set_encoding(data, ['time'], data_first.time.encoding)
-
-    return data
-
+    # Read the first file to get the time dimension
+    ds = xr.open_dataset(files_in[0], engine='netcdf4')
+    # Create a list of datasets
+    ds_list = []
+    for f in files_in:
+        ds_list.append(xr.open_dataset(f, engine='netcdf4'))
+    # Concatenate the list of datasets
+    ds = xr.concat(ds_list, dim=concat_dim)
+    # Identify duplicated time values
+    _, index = np.unique(ds.time, return_index=True)
+    # Keep only the unique time values
+    ds = ds.isel(time=index)
+    # Sort the dataset by time
+    ds = ds.sortby(concat_dim)
+    return ds
 
 def drop_duplicates(ds, dim):
     """drop duplicates from all data in ds for duplicates in dimension vector
@@ -112,3 +130,112 @@ def lists_to_np(indict):
         if isinstance(val, list):
             indict[key] = np.array(val)
     return indict
+
+class InstrumentSelector(object):
+    """Class that select and the instrument and list the files needed for the retrieval
+    It is now done outside of the retrieval class to prepare for parallel retrievals.
+
+    Essentially just running the original method "select_instrument" and "list_obs_files" from the retrieval class.
+
+    Args:
+        conf: configuration file or dictionary
+    """
+
+    def __init__(self, conf):
+        if isinstance(conf, dict):
+            self.conf = conf
+        elif os.path.isfile(conf):
+            self.conf = get_retrieval_config(conf)
+        else:
+            logger.error("The argument 'conf' must be a conf dictionary or a path pointing to a config file")
+            raise MWRConfigError("The argument 'conf' must be a conf dictionary or a path pointing to a config file")
+
+        # set by select_instrument():
+        self.wigos = None
+        self.inst_id = None
+        self.inst_conf = None
+
+        # set by list_obs():
+        self.mwr_files = None
+        self.alc_files = None
+
+    def select_oldest(self):
+        """select instrument which has oldest (processable) mwr file in input dir"""
+        # TODO: implement this
+        # TODO: Need to lock lookup for station selection for other nodes until prepare_eprofile_main with delete_mwr_in
+        #       is done. Something like https://stackoverflow.com/questions/52815858/python-lock-directory might work.
+        #       but better use ecflow to not data listing for other nodes until end of prepare_eprofile_main
+        # find oldest file in the folder and get station id from filename (or from file content)
+        # list files in input dir
+        list_of_files = glob.glob(os.path.join(self.conf['data']['mwr_dir'],
+                                               '{}*.nc'.format(self.conf['data']['mwr_file_prefix'])))
+        
+        if not list_of_files:
+            logger.error('No MWR data found in {}'.format(self.conf['data']['mwr_dir']))
+            raise MissingDataError('No MWR data found in {}'.format(self.conf['data']['mwr_dir']))
+        
+        # extract filename and dates of all files
+        list_of_file_date = [os.path.basename(x).split('/')[-1].split('_')[3] for x in list_of_files]
+        list_of_dates = [dt.datetime.strptime(x[1:-3], '%Y%m%d%H%M%S') for x in list_of_file_date]
+
+        # get oldest file based on the date in the filename:
+        id_oldest = list_of_dates.index(min(list_of_dates))
+        oldest_file = list_of_files[id_oldest]
+        
+        # get station id from filename
+        self.wigos = oldest_file.split('/')[-1].split('_')[2]
+        #self.wigos = '0-20008-0-INO'
+        
+        self.inst_id = oldest_file.split('/')[-1].split('_')[3][0]
+        #self.inst_id = 'A'
+
+        inst_conf_file = '{}{}_{}.yaml'.format(self.conf['data']['inst_config_file_prefix'],
+                                               self.wigos, self.inst_id)
+        self.inst_conf = get_inst_config(os.path.join(self.conf['data']['inst_config_dir'], inst_conf_file))
+
+    def set_instrument(self, wigos, inst_id):
+        """set instrument and config file manually providing wigos and inst_id"""
+        # TODO: implement this
+        # TODO: Need to lock lookup for station selection for other nodes until prepare_eprofile_main with delete_mwr_in
+        #       is done. Something like https://stackoverflow.com/questions/52815858/python-lock-directory might work.
+        #       but better use ecflow to not data listing for other nodes until end of prepare_eprofile_main
+
+        logger.info('Setting instrument to {} {}'.format(wigos, inst_id))
+
+        # set wigos and station id
+        self.wigos = wigos
+        self.inst_id = inst_id
+
+        # List files to check it some exist
+        list_of_files = glob.glob(os.path.join(self.conf['data']['mwr_dir'],
+                                                '{}*{}_{}*.nc'.format(self.conf['data']['mwr_file_prefix'],
+                                                                      self.wigos, self.inst_id)))
+        
+        if not list_of_files:
+            err_msg = 'No MWR data found in {}'.format(self.conf['data']['mwr_dir'])
+            logger.error(err_msg)
+            raise MissingDataError(err_msg)
+
+        inst_conf_file = '{}{}_{}.yaml'.format(self.conf['data']['inst_config_file_prefix'],
+                                               self.wigos, self.inst_id)
+        self.inst_conf = get_inst_config(os.path.join(self.conf['data']['inst_config_dir'], inst_conf_file))
+
+    def list_obs_files(self):
+        """get file lists for the selected station
+
+        Note:
+             this method shall list all (MWR) files not just the ones matching time settings. Like that old (obsolete)
+             files are removed when :meth:`prepare_obs` is run with delete_mwr_in=True
+        """
+        self.mwr_files = glob.glob(os.path.join(self.conf['data']['mwr_dir'],
+                                                '{}*{}_{}*.nc'.format(self.conf['data']['mwr_file_prefix'],
+                                                                      self.wigos, self.inst_id)))
+        self.alc_files = glob.glob(os.path.join(self.conf['data']['alc_dir'],
+                                                '{}*{}*.nc'.format(self.conf['data']['alc_file_prefix'], self.wigos)))
+        if not self.mwr_files:
+            err_msg = ('No MWR data for {} {} found in {}. These files must have been removed between station selection'
+                       ' and file listing. This should not happen!'.format(self.wigos, self.inst_id,
+                                                                           self.conf['data']['mwr_dir']))
+            logger.critical(err_msg)
+            raise MissingDataError(err_msg)
+
