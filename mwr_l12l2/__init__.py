e347e6cef2ad6fbe95b107c84977af97b8eb86bf
eric <e.sauvageat@gmail.com>
2026-02-12 13:50:52 +0000

    cleaned __init__

--- mwr_l12l2/__init__.py
+++ mwr_l12l2/__init__.py
@@ -1,10 +1,9 @@
 """mwr_l12l2: Tools for running optimal estimation retrievals for ground-based microwave radiometers."""
 
+import importlib.metadata
+
 # Import key classes/functions for convenience
 from mwr_l12l2.retrieval.retrieval import Retrieval
-from mwr_l12l2.retrieval.retrieval_manager import RetrievalManager
-
-import importlib.metadata
 
 __version__ = importlib.metadata.version("mwr_l12l2")
 
